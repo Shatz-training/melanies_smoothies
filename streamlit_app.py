97389cc55f1cbbe0305f9a9d4d7e239db351d03a
Shatz-training <ShatzC1+training@gmail.com>
2025-09-05 17:34:38 -0700

    Update streamlit_app.py
    Comment out additional debug write statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #convert to pandas dataframe
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 
 # multi select box for choosing fruit options
 ingredients_list = st.multiselect(
